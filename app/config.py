a9563baad24e5fec695a1c4386e95c7398b10419
Hossam <hosamameen948@gmail.com>
2026-08-06 11:25:08 +0300

    update version to be 2

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ else:
     # Standard Python execution: paths relative to source file
     BASE_DIR = Path(__file__).resolve().parent.parent
 
-APP_VERSION = "1.1.0"
+APP_VERSION = "2"
 UPLOAD_DIR = BASE_DIR / "uploads"
 OUTPUT_DIR = BASE_DIR / "outputs"
 
